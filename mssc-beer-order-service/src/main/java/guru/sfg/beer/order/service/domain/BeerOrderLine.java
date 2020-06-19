b6d63c20c6ad09b4271f835282cf391b2d996dc6
yul <yu.li@tecsys.com>
2020-06-19 08:42:35 -0400

    Making order service saving UPC. Closes #8

--- mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -19,12 +19,17 @@ import java.util.UUID;
 public class BeerOrderLine extends BaseEntity {
 
     @Builder
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
-                         Integer quantityAllocated) {
+    public BeerOrderLine(
+            UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+            BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
+            Integer quantityAllocated) {
+
         super(id, version, createdDate, lastModifiedDate);
+
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
+
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
@@ -33,6 +38,7 @@ public class BeerOrderLine extends BaseEntity {
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 }
