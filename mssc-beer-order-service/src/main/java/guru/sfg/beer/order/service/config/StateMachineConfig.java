292e0d6e00991d26fe809f657bf021babe039697
yul <yu.li@tecsys.com>
2020-08-24 08:47:11 -0400

    Config state transitions for order validation. Closes #41

--- mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/config/StateMachineConfig.java
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/config/StateMachineConfig.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
 import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.*;
+import static guru.sfg.beer.order.service.domain.BeerOrderEventEnum.*;
 
 
 @Slf4j
@@ -24,8 +26,9 @@ public class StateMachineConfig extends
         StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
 
     @Override
-    public void configure(StateMachineStateConfigurer<
-            BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+    public void configure(
+            StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum>
+                    states) throws Exception {
 
         states.withStates()
                 .initial(NEW)
@@ -36,6 +39,47 @@ public class StateMachineConfig extends
                 .end(VALIDATION_EXCEPTION)
                 .end(ALLOCATION_EXCEPTION)
                 .end(DELIVERY_EXCEPTION);
+    }
+
+    /*
+     * A transition is a relationship between a source state and a target state
+     * A switch from a state to another is a state transition caused by a trigger
+     *
+     * Internal Transition
+     *   - Internal transition is used when action needs to be executed without
+     *     causing a state transition
+     *   - With internal transition source and target state is always a same and
+     *     it is identical with self-transition in the absence of state entry
+     *     and exit actions
+     *
+     * External vs. Local Transition
+     *   - Most of the cases external and local transition are functionally
+     *     equivalent except in cases where transition is happening between
+     *     super and sub states
+     *   - Local transition doesn’t cause exit and entry to source state if
+     *     target state is a substate of a source state. Other way around,
+     *     local transition doesn’t cause exit and entry to target state if
+     *     target is a superstate of a source state.
+     */
+    @Override
+    public void configure(StateMachineTransitionConfigurer<
+            BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+
+        transitions.withExternal()
+                .source(NEW)
+                .target(NEW)
+                .event(VALIDATE_ORDER_EVENT)
+                // TODO: Add validation action here
+                .and()
+                .withExternal()
+                .source(NEW)
+                .target(VALIDATED)
+                .event(VALIDATION_PASSED_EVENT)
+                .and()
+                .withExternal()
+                .source(NEW)
+                .target(VALIDATION_EXCEPTION)
+                .event(VALIDATION_FAILED_EVENT);
     }
 
 }///:~
---
+++ mssc-beer-order-service/src/test/java/guru/sfg/beer/order/service/config/StateMachineConfigTest.java
@@ -0,0 +1,78 @@
+//: guru.sfg.beer.order.service.config.StateMachineConfigTest.java
+
+
+package guru.sfg.beer.order.service.config;
+
+
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@DisplayName("Test Order State Machine Configuration - ")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StateMachineConfigTest {
+
+    @Autowired
+    private StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum>
+            stateMachineFactory;
+
+    private StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> orderStateMachine;
+
+    @BeforeEach
+    void setUp() {
+        this.orderStateMachine = this.stateMachineFactory.getStateMachine(
+                UUID.randomUUID());
+    }
+
+    @Test
+    void test_State_Machine_Validation_Passed_Transitions() {
+
+        // Given
+        this.orderStateMachine.start();
+
+        BeerOrderStatusEnum beerOrderInitialStatus =
+                this.orderStateMachine.getState().getId();
+
+        this.orderStateMachine.sendEvent(BeerOrderEventEnum.VALIDATE_ORDER_EVENT);
+
+        this.orderStateMachine.sendEvent(BeerOrderEventEnum.VALIDATION_PASSED_EVENT);
+        BeerOrderStatusEnum beerOrderValidatedStatus =
+                this.orderStateMachine.getState().getId();
+
+        // Then
+        assertThat(beerOrderInitialStatus).isSameAs(BeerOrderStatusEnum.NEW);
+        assertThat(beerOrderValidatedStatus).isSameAs(BeerOrderStatusEnum.VALIDATED);
+    }
+
+    @Test
+    void test_State_Machine_Validation_Failed_Transitions() {
+
+        // Given
+        this.orderStateMachine.start();
+
+        BeerOrderStatusEnum beerOrderInitialStatus =
+                this.orderStateMachine.getState().getId();
+
+        this.orderStateMachine.sendEvent(BeerOrderEventEnum.VALIDATE_ORDER_EVENT);
+
+        this.orderStateMachine.sendEvent(BeerOrderEventEnum.VALIDATION_FAILED_EVENT);
+        BeerOrderStatusEnum beerOrderValidationExceptionStatus =
+                this.orderStateMachine.getState().getId();
+
+        // Then
+        assertThat(beerOrderInitialStatus).isSameAs(BeerOrderStatusEnum.NEW);
+        assertThat(beerOrderValidationExceptionStatus)
+                .isSameAs(BeerOrderStatusEnum.VALIDATION_EXCEPTION);
+    }
+
+}///:~
