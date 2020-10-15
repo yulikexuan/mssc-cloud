f909b502094b7b36c6ae0412a47a864eaaac406e
yul <yu.li@tecsys.com>
2020-10-15 08:01:24 -0400

    Config gateway route for beer service Closes #76

--- mssc-brewery-gateway/src/main/java/guru/sfg/brewery/gateway/config/LocalHostRouteConfig.java
+++ mssc-brewery-gateway/src/main/java/guru/sfg/brewery/gateway/config/LocalHostRouteConfig.java
@@ -8,11 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
 @Slf4j
-@Configuration
+// @Configuration
 public class LocalHostRouteConfig {
 
     static final String[] BEER_SERVICE_PATHS = {
